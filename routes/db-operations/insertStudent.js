b1170f058b01581b436e9266297ad1abc24779da
arvi <arvikonoha@gmail.com>
2020-01-13 04:13:36 +0530

    promisification successful

--- routes/db-operations/insertStudent.js
+++ routes/db-operations/insertStudent.js
@@ -1,6 +1,10 @@
 const dbConn = require('../../util/db-config')
 
-module.exports = async function insertStudent() {
+module.exports = async function insertStudent(usn,
+  fname,
+  lname,
+  department,
+  sem) {
   return new Promise((resolve, reject) => {
 
     dbConn.query(`insert into student values(?,?,?,?,?)`, [usn, fname, lname, department, sem], (err, result) => {
